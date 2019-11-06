1ee9965c020bb0ce26be866a06ecdd08814fdcf0
bagwanbulbul <bulbulbagwan918@gmail.com>
2019-11-06 12:15:00 +0530

    solve the probleam using switch case

---
+++ switch_case.js
@@ -0,0 +1,39 @@
+function getLetter(s) {
+    // let letter;
+    // Write your code here
+    switch(s){
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+            var later = "A";
+            return later;
+        case "b":
+        case "c":
+        case "d":
+        case "f":
+        case "g":
+            var later = "B";
+            return later;
+        case "j":
+        case "k":
+        case "l":
+        case "m":
+            var later = "C"
+            return later;
+        case "n":
+        case "p":
+        case "q":
+        case "r":
+        case "s":
+        case "t":
+        case "v":
+        case "w":
+        case "x":
+        
+            var later ="D"
+            return later;
+    }
+}
+getLetter(s)
