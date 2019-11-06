721da251704d2f8a9dd8a98a2e4fc76862e83687
bagwanbulbul <bulbulbagwan918@gmail.com>
2019-11-06 12:10:56 +0530

    sum of a very big arr

---
+++ AveryBigArr.js
@@ -0,0 +1,13 @@
+function aVeryBigSum(ar) {
+  var sum = 0;
+  for (var i = 0; i<ar.length; i++){
+    
+    sum = sum+ar[i]
+    
+    }
+    console.log(sum)
+    return sum
+
+
+}
+aVeryBigSum([1000000001,1000000002,1000000003,1000000004,1000000005])
